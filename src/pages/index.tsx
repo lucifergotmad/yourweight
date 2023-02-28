8a40c4cc5142c3c18486e7c43f2ce4630d80511e
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-02-28 15:30:02 +0700

    feat:[majo] update navbar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import Head from "next/head";
 
+import Navbar from "@/components/navbar.component";
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>YourWeight | Home Page</title>
+        <title>YourWeight | Landing Page</title>
       </Head>
+      <Navbar />
     </>
   );
 }
